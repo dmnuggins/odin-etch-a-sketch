f3cbd2148b26d5fe8243c46e5fad0376abb30f11
dylan <7257923+dmnuggins@users.noreply.github.com>
2023-07-07 17:00:37 -0700

    Grid redraw feature - Grid can now be redrawn by user input via modal popup

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ console.log("Etch-a-Sketch");
 const gridContainer = document.querySelector('.grid');
 
 function generateGrid(cols, rows) {
-    // var grid = document.createElement('div');
-    // grid.className = 'grid';
+
 
     for (var i = 0; i < cols; i++) {
         var column = document.createElement('div'); // generate column
@@ -12,19 +11,45 @@ function generateGrid(cols, rows) {
         for (var j = 0; j < rows; j++) {
             var row = document.createElement('div'); // generate row
             row.className = 'row';
-            row.textContent = (i + 1) + '-' + (j + 1);
+            // row.textContent = (i + 1) + '-' + (j + 1);
             column.appendChild(row);
         }
         gridContainer.appendChild(column);
     }
 }
 
-function updateGridContainer(rows, cols) {
-    const width = cols * 10;
-    const height = rows * 10;
-    gridContainer.style.width = String(width) + 'px';
-    gridContainer.style.height = String(height) + 'px';
-}
-
 generateGrid(19,16);
-// generateGrid(16,10);
+
+// Modal
+const modal = document.querySelector("#modal");
+const openModal = document.querySelector(".redraw-button");
+const closeModal = document.querySelector(".close-button");
+
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
+
+// Get data from Modal
+const form = document.querySelector('.form');
+const colsInput = document.getElementById('colsInput');
+const rowsInput = document.getElementById('rowsInput');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const columns = parseInt(colsInput.value, 10);
+
+    const rows = parseInt(rowsInput.value, 10);
+    console.log(columns);
+    console.log(rows);
+
+    while(gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    generateGrid(columns, rows);
+})
